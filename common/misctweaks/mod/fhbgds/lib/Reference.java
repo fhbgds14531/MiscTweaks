ad6c62eb39c41dae66c2ecc42619e978793c26e7
fhbgds14531 <rmshamannjam@gmail.com>
2013-08-06 19:31:48 -0700

    Updated version

--- common/misctweaks/mod/fhbgds/lib/Reference.java
+++ common/misctweaks/mod/fhbgds/lib/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 
 	public static final String MOD_ID = "misctweaks";
 	public static final String MOD_NAME = "MiscTweaks";
-	public static final String VERSION = "0.5.1";
+	public static final String VERSION = "1.5";
 	
 }
